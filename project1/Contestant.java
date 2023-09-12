ce7201fa00b04ebf6ac4f5dbbde29eb4e8f7bc43
dshay22 <dorianshay0806@gmail.com>
2023-09-12 09:59:54 -0700

    Made a project 1 folder space

---
+++ project1/Contestant.java
@@ -0,0 +1,38 @@
+package project1;
+public class Contestant {
+    private String name;
+    private int score;
+
+    public Contestant(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public int compareTo(Contestant obj){
+        
+        return -1;
+    }
+
+    public String toString(Contestant obj){
+        String formattedString;
+        String playerName = obj.name;
+        String playerScore = string_to_int(obj.score);
+
+        formattedString = playerName + ": " + playerScore;
+
+        return formattedString;
+    }
+
+    public String string_to_int(int x){
+       return Integer.toString(x);
+    }
+
+}
---
+++ project1/LeaderBoardTest.java
@@ -0,0 +1,12 @@
+package project1;
+import java.util.Scanner;
+
+class LeaderBoardTest {
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        
+    }
+
+}
