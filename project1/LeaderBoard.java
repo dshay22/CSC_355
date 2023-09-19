f4667dec35004f7ece1358646b3d2cdf1ca01731
dshay22 <dorianshay0806@gmail.com>
2023-09-19 09:09:28 -0700

    Fixing spacing issues

--- project1/LeaderBoard.java
+++ project1/LeaderBoard.java
@@ -24,7 +24,7 @@ public class LeaderBoard {
           for(int i = 0; i < leaderBordSize; ++i){
                if (leaderBord[i].compareTo(c) > 0) {
                     for (int j = leaderBord.length - 1; j > i; j--) {
-                    leaderBord[j] = leaderBord[j - 1];
+                         leaderBord[j] = leaderBord[j - 1];
                     }
                     leaderBord[i] = c;
                     break;
