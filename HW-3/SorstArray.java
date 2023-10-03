98974e2269cbb7d0ae679fa8052f5956312afe63
dshay22 <dorianshay0806@gmail.com>
2023-10-03 10:11:49 -0700

    added comments to problem 2

--- HW-3/SorstArray.java
+++ HW-3/SorstArray.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class SorstArray {
     public static void main(String[] args) {
-        int[] exArry = {0, 2, 5, 6, 7, 8, 10, 2, 8, 9};
+        int[] exArry = {0, 2, 5, 6, 7, 8, 8, 2, 4, 9};
         int splitIndex = 6;
 
         int[] newArry = sortArray(exArry, splitIndex);  //Call my sorting function
@@ -19,20 +19,20 @@ public class SorstArray {
         int _m = 0; //Index for the merged array
 
             //Merge the subarrays
-            while(_a <= splitIndex && _b < inputArry.length){
-                if(inputArry[_a] < inputArry[_b]){
+            while(_a <= splitIndex && _b < inputArry.length){ //moves through each of the sub-arrays and compares there values
+                if(inputArry[_a] < inputArry[_b]){           //if the first subarray is smaller then add to new array
                     B[_m++] = inputArry[_a++];
 
                 }
-                else{
+                else{                                     //if second sub-array is smaller add this value to new array
                     B[_m++] = inputArry[_b++];
                 }
             }
-            for(int i = _b; i < inputArry.length; ++i){
+            for(int i = _b; i < inputArry.length; ++i){  //if there are remaining values in the sub arrays add them
                 B[_m++] = inputArry[_b++];
             }
-            for(int i = _a; i <= splitIndex; ++i){
-                B[_m++] = inputArry[_a++];
+            for(int i = _a; i <= splitIndex; ++i){      //if there are remaining values in the sub arrays add them
+                B[_m++] = inputArry[_a++]; 
             }
 
         return B;
