7446d69191fc8291ddba964cbccf553f918c458b
dshay22 <dorianshay0806@gmail.com>
2023-10-26 08:47:36 -0700

    Starting a quick sort for a 2D array

--- project3/SortGrid.java
+++ project3/SortGrid.java
@@ -1,3 +1,53 @@
-public class SortGrid {
-    
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SortGrid extends SortingTest{
+
+    private int[][] arry2D;
+    private int dimension = 0;
+
+    SortGrid(int gridSize, Scanner fileReader){
+        dimension = gridSize;                     //initialize size var
+        arry2D = new int[gridSize][gridSize];     //initialize size array
+
+        for(int i = 0; i < dimension; ++i){       //populate array
+            for(int j = 0; j < dimension; ++j){
+                if(fileReader.hasNextInt()){
+                    arry2D[i][j] = fileReader.nextInt();
+                }
+            }
+        }
+
+    }
+
+    public void printArray(){
+
+        for(int i = 0; i < dimension; ++i){
+
+            for(int j = 0; j < dimension; ++j){
+                    System.out.print(arry2D[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public void sort(){ //implimented quick sort for part 3 of project 3
+        int pivot = dimension / 2;
+
+        int temp = arry2D[pivot - 1][dimension - 1];
+        arry2D[pivot - 1][dimension - 1] = arry2D[dimension - 1][dimension - 1];
+        arry2D[dimension - 1][dimension - 1] = temp;
+
+        for(int i = 0; i < dimension; ++i){       //populate array
+            for(int j = 0; j < dimension; ++j){
+                if(arry2D[i][j] < arry2D[dimension - 1][dimension - 1]){
+                    
+                }
+            }
+        }
+    }
+
+
 }
--- project3/SortingTest.java
+++ project3/SortingTest.java
@@ -14,7 +14,14 @@ public class SortingTest{
     private File arr4 = new File("array4.txt");
     private File arr5 = new File("array5.txt");
 
+    private File grid1 = new File("testGrid1.txt");
+    private File grid2 = new File("testGrid2.txt");
+    private File grid3 = new File("testGrid3.txt");
+    private File grid4 = new File("testGrid4.txt");
+    private File grid5 = new File("testGrid5.txt");
+
     private File[] myFileArry = { arr1, arr2, arr3, arr4, arr5};
+    private File[] myFileGrid = { grid1};//, grid2, grid3, grid4, grid5};
 
     private Scanner scnr;
 
@@ -40,21 +47,35 @@ public class SortingTest{
 
         ShellSort shellObj = new ShellSort();
         ArraySort arrayObj = new ArraySort();
+        int gridSize;
 
         try {
-            for(File newFile : myFileArry){ //for shell sort
-                shellObj.arry = readFile(scnr, newFile);
-                shellObj.sort(shellObj.arry);
-                System.out.println("Sorted Array: " + shellObj.arry + "\n");
-                System.out.println("\n\n :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: \n\n");
+            // for(File newFile : myFileArry){ //for shell sort
+            //     shellObj.arry = readFile(scnr, newFile);
+            //     shellObj.sort(shellObj.arry);
+            //     System.out.println("Sorted Array: " + shellObj.arry + "\n");
+            //     System.out.println("\n\n :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: \n\n");
+            // }
+
+            // for(File newFile : myFileArry){// for ArraySort wich I chose to impliment merge sort
+            //     arrayObj.arry = readFile(scnr, newFile);
+            //     arrayObj.sort(arrayObj.arry);
+            //     System.out.println("Sorted Array: " + arrayObj.arry + "\n");
+            //     System.out.println("\n\n :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: \n\n");
+            // }
+            
+            
+            for(File newGrid : myFileGrid){
+                scnr = new Scanner(newGrid);
+                gridSize = scnr.nextInt();
+                SortGrid gridObj = new SortGrid(gridSize, scnr);
+                gridObj.printArray();
+                gridObj.sort();
+                System.out.println();
+                gridObj.printArray();
+
             }
 
-            for(File newFile : myFileArry){// for ArraySort wich I chose to impliment merge sort
-                arrayObj.arry = readFile(scnr, newFile);
-                arrayObj.sort(arrayObj.arry);
-                System.out.println("Sorted Array: " + arrayObj.arry + "\n");
-                System.out.println("\n\n :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: \n\n");
-            }
         } 
         catch (Exception  e) {
             e.printStackTrace();
